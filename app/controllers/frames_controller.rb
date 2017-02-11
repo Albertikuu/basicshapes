5baf924de144ecfd742053704271b9996cbeecea
lorenzo.ita@gmail.com <lorenzo.ita@gmail.com>
2017-02-11 17:58:58 +0100

    adds empty home page frames#index

---
+++ app/controllers/frames_controller.rb
@@ -0,0 +1,16 @@
+class FramesController < ApplicationController
+# before_action load_categories
+	
+	def index
+
+	end
+
+
+private
+
+	def load_categories
+		@categories = Category.all
+	end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :categories
+  root 'frames#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
