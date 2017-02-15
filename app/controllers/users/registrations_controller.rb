6b4535191f28f46661bcd9473e260b4a8e69b2ff
lorenzo.ita@gmail.com <lorenzo.ita@gmail.com>
2017-02-15 09:46:40 +0100

    santize fields for registration in signup

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,4 +8,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
     super
   end
 
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :job_title, :location)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :job_title, :location)
+  end
+  
+#http://jacopretorius.net/2014/03/adding-custom-fields-to-your-devise-user-model-in-rails-4.html
+
 end 
