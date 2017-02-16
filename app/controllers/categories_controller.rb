718d984aa1b62a9f649e2c4f78858cd07d863e4b
lorenzo.ita@gmail.com <lorenzo.ita@gmail.com>
2017-02-16 13:41:16 +0100

    allows logged users to add others to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,17 @@
 class CategoriesController < ApplicationController
 
 	def show 
-		@category = Category.find_by(title: params[:title])
-		
+		@all_users = User.where.not(id: current_user.id)
+		@category = Category.find_by(title: params[:title])		
+	end
+
+	def index
+		if session[:categories]
+			render json: session[:categories]
+		end
 	end
 
 	def new
-		@category = Category.new
 	end
 
 	def create
@@ -17,6 +22,12 @@ class CategoriesController < ApplicationController
 
 	def destroy
 		@category = Category.find_by(title: params[:title]).delete
+		redirect_to('/')
+	end
+
+	def add_user
+		user = User.find_by(id: params[:user_id])
+		Category.find_by(title: params[:title]).users << user
 		redirect_to('/')
 	end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :users, path: '',
   	path_names: {sign_in: 'login', sign_up: 'signup'}, 
   	controllers: {sessions: 'users/sessions', registrations: 'users/registrations'}
- 	resources :users
-  # get '/categories/:title', to: 'categories#show', as: 'categories'
-  # resources :categories, except: :index, except: :show
-  resources :categories, param: :title, except: :index
-    root 'frames#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :users
+  resources :categories, param: :title #, except: :index
+  post '/categories/add_user/:title/:user_id', as: 'category_add_user', to: 'categories#add_user'
+  root 'frames#index'
+
 end
