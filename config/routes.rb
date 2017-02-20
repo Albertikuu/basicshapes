49049ac9c6c2b4f9f6cf78eabccb1e1909edbed1
lorenzo.ita@gmail.com <lorenzo.ita@gmail.com>
2017-02-20 15:47:06 +0100

    partial nested route of category into team

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   	controllers: {sessions: 'users/sessions', registrations: 'users/registrations'}
   
   resources :users
-  resources :teams, param: :name, except: :index do
-  	resources :categories, param: :title #, except: :index
+  resources :categories, param: :title, except: [:index, :show]
+  resources :teams, param: :name do
+  	resources :categories, param: :title, only: [:index, :show]
   end 
 
   post '/categories/add_user/:title/:user_id', as: 'category_add_user', to: 'categories#add_user'
