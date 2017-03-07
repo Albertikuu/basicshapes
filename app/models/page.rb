f6f9ffacaa98364b8cb196b3d8bf7d5d6ec24a7f
Albert Ramirez <albert.ramirez.canalias@gmail.com>
2017-03-07 14:36:13 +0100

    Make some visual changes

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,11 +2,10 @@ class Page < ApplicationRecord
 belongs_to :category
 has_many :versions
 has_many :commits
-	
+
  validates_presence_of :slug
  validates :slug, uniqueness: { scope: :category_id,
     message: "this page already exists", case_sensitive: false }
-
 
 
 end
