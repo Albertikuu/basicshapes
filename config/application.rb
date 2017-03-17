1fda7a9928209306e7d2df94a624b2fc85f48444
lorenzo.ita@gmail.com <lorenzo.ita@gmail.com>
2017-03-17 12:44:02 +0100

    touches user show and add conditional of development for dotenv

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,9 @@ require_relative 'boot'
 
 require 'rails/all'
 
+if Rails.env.development?
+require 'dotenv/load' #only in development
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
