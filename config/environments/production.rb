846ba4ee09c71630715c4c6e63d8acf8ccdcec89
lorenzo.ita@gmail.com <lorenzo.ita@gmail.com>
2017-03-24 16:12:11 +0100

    adds default url to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,19 +52,13 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
-  config.action_mailer.default_url_options = {:host => 'herokuapp.com'}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => "127.0.0.1",
-    :port    => "587",
-    :domain  => 'herokuapp.com'
-  }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "basicshapes_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.perform_deliveries = true
+   config.action_mailer.default_url_options = {:host => ENV['SENDGRID_DOMAIN'], :protocol => 'http'} #I've also tried it without ":protocol => 'http'"
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
