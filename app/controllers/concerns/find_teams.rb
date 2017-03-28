fc0f22ca833e6a07231c768303959894b66be5a4
lorenzo.ita@gmail.com <lorenzo.ita@gmail.com>
2017-03-28 16:41:28 +0200

    modifies find_team module to always set session current team - tentative

--- app/controllers/concerns/find_teams.rb
+++ app/controllers/concerns/find_teams.rb
@@ -1,9 +1,13 @@
 module Find_teams
 
 	def find_teams
+
 		if user_signed_in? && current_user.teams.size == 0
 			redirect_to '/teams/find'
+		elsif user_signed_in? && current_user.teams.size != 0
+			session[:current_team] = current_user.teams.first
 		end
+			
 	end	
-
+	
 end
