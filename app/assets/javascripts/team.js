31874e56fcbccf99556a71b9e4d2742b5a29aba8
lorenzo.ita@gmail.com <lorenzo.ita@gmail.com>
2017-04-03 17:12:45 +0200

    updates teams.js

--- app/assets/javascripts/team.js
+++ app/assets/javascripts/team.js
@@ -8,8 +8,47 @@ function createTeam(event){
             members: participants
         });
 
-	request.fail(console.log('the category request failed'));
- 	request.done(console.log('the category request was succesful'))
+	request.fail(function(error){
+		console.log(error)
+	});
+ 	request.success(function(data){
+ 		console.log('the category request was succesful', data)
+ 	});
 
 };
+
+
+function addUserToTeam(event){
+	event.preventDefault();
+
+	var participants = new Array();
+	participants = $('#participants-invite-js').val().split(',');
+    request = $.post("/teams/invite_users", {
+            members: participants,
+            team_slug: $('.invite-users-js').data('team')
+        });
+
+	request.fail(function(error){
+		console.log(error)
+	});
+ 	request.success(function(data){
+ 		console.log('the category request was succesful', data)
+ 	});
+
+};
+
+function removeMember(event){
+	event.preventDefault;
+    request = $.post("/teams/remove_member", {
+            user_id: event.currentTarget.id,
+            team_slug: event.currentTarget.dataset.team
+        });
+
+	request.fail(function(error){
+		console.log(error)
+	});
+ 	request.success(function(data){
+ 		console.log('the team request was succesful', data)
+ 	});
+}
 
